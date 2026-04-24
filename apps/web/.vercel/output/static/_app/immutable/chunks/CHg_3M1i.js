23a5522840c2669e73d99002d8a9f95f79345153
theNeuralHorizon <kshitij.betwal@gmail.com>
2026-04-25 00:52:21 +0530

    chore(deploy): gitignore apps/web/.vercel build output

--- apps/web/.vercel/output/static/_app/immutable/chunks/CHg_3M1i.js
+++
@@ -1,2 +0,0 @@
-import{f,z as e,x as t}from"./DwPB1CvL.js";function c(n){throw new Error("https://svelte.dev/e/lifecycle_outside_component")}function u(n){e===null&&c(),f(()=>{const o=t(n);if(typeof o=="function")return o})}function r(n){e===null&&c(),u(()=>()=>t(n))}export{r as a,u as o};
-//# sourceMappingURL=CHg_3M1i.js.map
--- apps/web/.vercel/output/static/_app/immutable/entry/start.C7BABjx0.js
+++
@@ -1,2 +0,0 @@
-import{l as o,a as r}from"../chunks/8BXQXmHP.js";export{o as load_css,r as start};
-//# sourceMappingURL=start.C7BABjx0.js.map
